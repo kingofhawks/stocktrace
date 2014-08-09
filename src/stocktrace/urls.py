e95a53189f0512b635df676042a90f395a40fd9d
kingofhawks <kingofhawks@qq.com>
2014-08-09 12:59:04 +0800

    upgrade to bootstrap3.2
    upgrade to bootstrap3.2 and remove redundant templates

--- src/stocktrace/urls.py
+++ src/stocktrace/urls.py
@@ -15,14 +15,14 @@ urlpatterns = patterns('',
     url(r'^zytj/candle/(\d{6})/$', 'zytj.views.candlestick'),
     #NHNL Index
     url(r'^zytj/jsonnhnl', 'zytj.views.jsonnhnl'),
-    url(r'^zytj/nhnl', 'zytj.views.nhnl'),
+    url(r'^zytj/nhnl', 'zytj.views.nhnl', name='zytj.nhnl'),
     #MA Index
-    url(r'^zytj/ma', 'zytj.views.ma'),
+    url(r'^zytj/ma', 'zytj.views.ma', name='zytj.ma'),
     #Order by 52 week high/low percentage
     url(r'^zytj/alist/ma/(\d{2})/$', 'zytj.views.alist_days_ma'),#asc by days on MA
     url(r'^zytj/alist/(\d{2})/$', 'zytj.views.alist_days'),#asc by days on MA
-    url(r'^zytj/alist', 'zytj.views.ascendinglist'),  #asc from year low
-    url(r'^zytj/dlist', 'zytj.views.descendinglist'), #des from year high
+    url(r'^zytj/alist', 'zytj.views.ascendinglist', name='zytj.alist'),  #asc from year low
+    url(r'^zytj/dlist', 'zytj.views.descendinglist', name='zytj.dlist'), #des from year high
     url(r'^zytj/quotes', 'zytj.views.quotes'),#show all quotes
     url(r'^zytj/delete', 'zytj.views.delete_stock'),#delete stock
     
