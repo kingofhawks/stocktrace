ea854d9ac44a03fd1cdca982b049c776bb74b63f
smartman <smartman2014@qq.com>
2018-03-12 17:01:46 +0800

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chart from './Chart'
+import BasicExample from './Router'
 
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save it to reload.
         </p>
-
+        <BasicExample/>
           <Chart code={code}/>
       </div>
     );
