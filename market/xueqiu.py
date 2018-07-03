a79cbf1c02765ebc60ba9c1d92ca58e22a9ec46f
kingofhawks <kingofhawks@qq.com>
2018-07-03 21:55:48 +0800

    add magic api

--- market/xueqiu.py
+++ market/xueqiu.py
@@ -24,7 +24,7 @@ from django.conf import settings
 db = settings.DB
 api_home = 'http://xueqiu.com'
 # check xueqiu HTTP request cookie "xq_a_token"
-xq_a_token = 'b3dd9fc0abeb2ef6635a9cae910cdd00117b3773'
+xq_a_token = '3361bd8b21cb5650bd593b67d8a66f2c6667a20c'
 headers = {'content-type': 'application/json',
            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'}
 
