f7cf32b8d676b1403fac3541c4f67ceb7c28c795
kingofhawks <kingofhawks@qq.com>
2018-08-23 22:02:33 +0800

    更新ROE时有问题

--- market/tushare.py
+++ market/tushare.py
@@ -36,8 +36,11 @@ def finance_report(year=2018, quarter=2):
     print(df)
     data = df.to_dict('index')
     print(data)
+    from mongoengine.queryset.visitor import Q
     for index, value in sorted(data.items()):
         code = value['code']
         roe = value['roe']
-        Equity.objects(code=code, date=date).update_one(code=code, date=date, roe=roe, upsert=True)
-
+        # Equity.objects(code=code).update(code=code, roe=roe, upsert=True)
+        # Equity.objects(__raw__={"code":code,"date" : {"$gte":'2018-07-01',"$lte":'2018-08-31'}}).update(code=code, roe=roe, upsert=True)
+        # 只更新最近一个季度的数据
+        Equity.objects(Q(code=code) & Q(date__gte='2018-07-01') & Q(date__lte='2018-08-31')).update(roe=roe)
