b86818274200e8cd4d032fdd9b4bf334529e42cf
kingofhawks <kingofhawks@qq.com>
2019-08-31 20:09:03 +0800

    parse choice data

--- market/choice.py
+++ market/choice.py
@@ -1,13 +1,17 @@
 import pandas as pd
 from market.models import Index
 import numpy as np
+import math
 
 code = {'000015.SH': '红利指数', '000016.SH': '上证50', '000300.SH':'沪深300', '000905.SH': '中证500',
-        '399005.SZ': '中小板', '399006.SZ': '创业板'}
+        '399005.SZ': '中小板', '399006.SZ': '创业板','399102.SZ': '创业板综',
+        '000918.SH': '300成长', '000919.SH': '300价值', '000922.SH': '中证红利',
+        '000925.SH': '基本面50', '000932.SH': '中证消费', '000934.SH': '中证金融',
+        'DJIA.GI': '道琼斯工业', 'HSI.HI': '恒生指数', 'IXIC.GI': '纳斯达克', 'SPX.GI': '标普500'}
 
 
 def index_stats(file):
-    file_name = 'D:\workspace\stocktrace\market\{}.xls'.format(file)
+    file_name = 'D:\workspace\stocktrace\data\{}.xls'.format(file)
     # set sheet_name to None will read all sheets to map of DF
     # index_col 2 will read the 2nd column as index(时间列)
     #  '--' is recognized as None
@@ -20,8 +24,7 @@ def index_stats(file):
         df = df.dropna(subset=['PE(TTM)'])
         # print(df.columns)
         # print(df.index)
-        # get last 5 Year data
-        # print(df)
+        # 取最近5年数据
         df = df.last('1825D')
         # df = df.last('5Y')
         # print(df.tail(1))
@@ -37,11 +40,16 @@ def index_stats(file):
         min_pe = df['PE(TTM)'].min()
         mean = df['PE(TTM)'].mean()
         median = df['PE(TTM)'].median()
+        # 30分位
         p30 = df['PE(TTM)'].quantile(0.3)
+        # 70分位
         p70 = df['PE(TTM)'].quantile(0.7)
+        # a = [max_pe, min_pe, mean, median, p30, p70]
+        # percent = np.percentile(a, last_pe)
         # 保留两位小数输出
-        print('{} PE max:{:.2f} min:{:.2f} mean:{:.2f} median:{:.2f} 30%:{:.2f} 70%:{:.2f} current:{:.2f} 红绿灯:{}'
-              .format(code.get(sheet), max_pe, min_pe, mean, median, p30, p70, last_pe, last_pe<p30))
+        print('{} PE max:{:.2f} min:{:.2f} mean:{:.2f} median:{:.2f} 30分位%:{:.2f} 70分位%:{:.2f} '
+              'current:{:.2f} 红绿灯:{}'
+              .format(code.get(sheet), max_pe, min_pe, mean, median, p30, p70, last_pe, last_pe < p30))
 
 
 @DeprecationWarning
@@ -83,5 +91,10 @@ def index_stats_2(code):
 
 
 if __name__ == '__main__':
-    index_stats('choice')
+    # 10年10倍求年化收益率为25.89%,使用开方运算
+    print(10**(1/10)-1)
+    a = [40.7, 20.52, 28.12, 27.66, 25.89, 29.85]
+    # 求数组中50分位的数值
+    print(np.percentile(a, 50))
+    index_stats('choice20190831')
     # index_stats_2('000300')
